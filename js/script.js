e26315f51e8c40926d0ffaf6e8cc92d020de7203
Gabriel Cestari <gabrielnaruto.rinnegan@gmail.com>
2019-10-24 17:02:13 -0300

    Alterando estilo/index e js.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,47 @@
-            // Função para que o menu apareça na rolagem
-            var prevScrollpos = window.pageYOffset;
-            window.onscroll = function() {
-                    var currentScrollPos = window.pageYOffset;
-                    if (prevScrollpos > currentScrollPos) {
-                        document.getElementById("navbar").style.top = "0";
-                    } else {
-                        document.getElementById("navbar").style.top = "-100px";
-                    }
-                    prevScrollpos = currentScrollPos;
-                }
-                // Fim da função rolagem
+                       // Função de loading da tela
+                       var i = setInterval(function loading() {
+                           clearInterval(i);
 
-            // Inicio da função deslize suave da página
-            $(function() {
-                $('a').bind('click', function(event) {
-                    var $anchor = $(this);
-                    $('html, body').stop().animate({
-                        scrollTop: $($anchor.attr('href')).offset().top
-                    }, 1000, 'swing');
-                });
-            });
-            // Fim da função deslize
+                           $('#load').css('display', 'none');
+                       }, 2000);
 
-            // Função para o setor serviços
-            var section = $('li');
+                       // Função para que o menu apareça na rolagem
+                       var prevScrollpos = window.pageYOffset;
+                       window.onscroll = function() {
+                               var currentScrollPos = window.pageYOffset;
+                               if (prevScrollpos > currentScrollPos) {
+                                   document.getElementById("navbar").style.top = "0";
+                               } else {
+                                   document.getElementById("navbar").style.top = "-100px";
+                               }
+                               prevScrollpos = currentScrollPos;
+                           }
+                           // Fim da função rolagem
 
-            function toggleAccordion() {
-                if ($(this).hasClass('active')) {
-                    $(this).removeClass('active');
-                    $(".accordion_principal > li").removeClass("nohover");
+                       // Inicio da função deslize suave da página
+                       $(function() {
+                           $('a').bind('click', function(event) {
+                               var $anchor = $(this);
+                               $('html, body').stop().animate({
+                                   scrollTop: $($anchor.attr('href')).offset().top
+                               }, 1000, 'swing');
+                           });
+                       });
+                       // Fim da função deslize
 
-                } else {
-                    section.removeClass('active');
-                    $(this).addClass('active');
-                    $(".accordion_principal > li").addClass("nohover");
-                    $(this).removeClass('nohover');
-                }
-            }
-            section.on('click', toggleAccordion);
+                       // Função para o setor serviços
+                       var section = $('li');
+
+                       function toggleAccordion() {
+                           if ($(this).hasClass('active')) {
+                               $(this).removeClass('active');
+                               $(".accordion_principal > li").removeClass("nohover");
+
+                           } else {
+                               section.removeClass('active');
+                               $(this).addClass('active');
+                               $(".accordion_principal > li").addClass("nohover");
+                               $(this).removeClass('nohover');
+                           }
+                       }
+                       section.on('click', toggleAccordion);
