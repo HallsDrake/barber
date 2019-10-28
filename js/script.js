fe20acb88dbb1d3f253795a356591900195c52f3
Gabriel Cestari <gabrielnaruto.rinnegan@gmail.com>
2019-10-28 17:08:52 -0300

    Efeitos de transição aplicados

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
                        // Função para que o menu apareça na rolagem
+
                        var prevScrollpos = window.pageYOffset;
                        window.onscroll = function() {
                                var currentScrollPos = window.pageYOffset;
@@ -12,6 +13,7 @@
                            // Fim da função rolagem
 
                        // Inicio da função deslize suave da página
+
                        $(function() {
                            $('a').bind('click', function(event) {
                                var $anchor = $(this);
@@ -21,3 +23,39 @@
                            });
                        });
                        // Fim da função deslize
+
+                       const bounce = function(func, wait, immediate) {
+                           let timeout;
+                           return function(...args) {
+                               const context = this;
+                               const later = function() {
+                                   timeout = null;
+                                   if (!immediate) func.apply(context, args);
+                               };
+                               const callNow = immediate && !timeout;
+                               clearTimeout(timeout);
+                               timeout = setTimeout(later, wait);
+                               if (callNow) func.apply(context, args);
+                           };
+                       };
+
+                       const target = document.querySelectorAll('[data-anime]');
+                       const animationClass = 'animate';
+
+                       function animeScroll() {
+                           const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+                           target.forEach(function(element) {
+                               if ((windowTop) > element.offsetTop) {
+                                   element.classList.add(animationClass);
+                               } else {
+                                   element.classList.remove(animationClass);
+                               }
+                           })
+                       }
+
+                       animeScroll();
+                       if (target.length) {
+                           window.addEventListener('scroll', function() {
+                               animeScroll();
+                           })
+                       }
