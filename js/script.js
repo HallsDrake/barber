62d429dd4d97f754b392a14120a8ba3ce50d4b72
Gabriel Cestari <gabrielcolares.ti@gmail.com>
2019-10-29 02:39:19 -0300

    Adicionado os efeitos de transição

--- js/script.js
+++ js/script.js
@@ -24,21 +24,6 @@
                        });
                        // Fim da função deslize
 
-                       const bounce = function(func, wait, immediate) {
-                           let timeout;
-                           return function(...args) {
-                               const context = this;
-                               const later = function() {
-                                   timeout = null;
-                                   if (!immediate) func.apply(context, args);
-                               };
-                               const callNow = immediate && !timeout;
-                               clearTimeout(timeout);
-                               timeout = setTimeout(later, wait);
-                               if (callNow) func.apply(context, args);
-                           };
-                       };
-
                        const target = document.querySelectorAll('[data-anime]');
                        const animationClass = 'animate';
 
